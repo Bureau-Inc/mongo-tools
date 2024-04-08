a9ef423b0d9dc6e62ae36140ee98a0b78e1e12d2
jay-at-bureau <jay.bhatt@bureau.id>
2024-04-08 12:02:21 +0530

    GP-1829: Updated test case.

--- mongoexport/custom_export_document_test.go
+++ mongoexport/custom_export_document_test.go
@@ -10,16 +10,20 @@ import (
 
 func TestExportDocumentCustom(t *testing.T) {
 	output := bytes.NewBuffer([]byte{})
-	csvOut := NewCSVExportOutput([]string{"f1", "f2", "f3", "f4"}, true, output)
+	csvOut := NewCSVExportOutput([]string{"f1", "f2", "f3", "f4", "s1", "s2"}, true, output)
 	doc := primitive.D{
 		primitive.E{Key: "f1", Value: float64(1.234)},
 		primitive.E{Key: "f2", Value: float64(1)},
 		primitive.E{Key: "f3", Value: float32(-12334.2433)},
 		primitive.E{Key: "f4", Value: float64(844737823984723.4430948230)},
+		primitive.E{Key: "s1", Value: "some val"},
+		primitive.E{Key: "s2", Value: primitive.M{"nested_key": "nested_val"}},
 	}
 	err := csvOut.ExportDocumentCustom(doc)
-	assert.NoError(t, err)
+	if !assert.NoError(t, err) {
+		return
+	}
 	csvOut.Flush()
 
-	assert.Equal(t, "1.234,1,-12334.2431640625,844737823984723.5\n", output.String())
+	assert.Equal(t, "1.234,1,-12334.2431640625,844737823984723.5,some val,\"{\"\"nested_key\"\":\"\"nested_val\"\"}\"\n", output.String())
 }
